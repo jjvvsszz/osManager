d8584b3061b8767b647ea3d576ced1c10e7f4ef3
João Victor de Sousa Zupa <contato@jaooo.tk>
2025-10-22 15:54:50 -0300

    Initial Commit

---
+++ src/main/java/tk/jaooo/osmanager/OsManagerApplication.java
@@ -0,0 +1,13 @@
+package tk.jaooo.osmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OsManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tk/jaooo/osmanager/OsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package tk.jaooo.osmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
