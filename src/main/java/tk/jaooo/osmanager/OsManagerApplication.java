6a1578c58345bdb17d7756affc4f9aa807f2205b
João Victor de Sousa Zupa <contato@jaooo.tk>
2025-10-24 10:52:53 -0300

    Banco conectado

--- src/main/java/tk/jaooo/osmanager/OsManagerApplication.java
+++ src/main/java/tk/jaooo/osmanager/OsManagerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OsManagerApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(OsManagerApplication.class, args);
     }
 
