ec37d40d536376aaac9e3cea4478391bf8322911
João Victor de Sousa Zupa <contato@jaooo.tk>
2025-12-25 19:36:11 -0300

    feat: remove unused import for GeminiService in OsManagerApplicationTests

--- src/test/java/tk/jaooo/osmanager/OsManagerApplicationTests.java
+++ src/test/java/tk/jaooo/osmanager/OsManagerApplicationTests.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import tk.jaooo.osmanager.services.GeminiService;
 import tk.jaooo.osmanager.services.OciSecretsService;
 
 @SpringBootTest
